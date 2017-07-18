a8f546c3ae12d3ea39089681841120317061f737
195Iridium77 <matt.martin0108@gmail.com>
2017-07-18 20:56:02 +0930

    Using Jinja Objects

--- headlines.py
+++ headlines.py
@@ -16,9 +16,7 @@ def get_news(publication="sciam"):
     feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed['entries'][0]
 
-    return render_template("home.html", title=first_article.get("title"),
-                            published=first_article.get("published"),
-                            summary=first_article.get("summary"))
+    return render_template("home.html", article=first_article)
 
 
 if __name__ == '__main__':
