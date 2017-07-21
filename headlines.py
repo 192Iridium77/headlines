f9d41175d15372d98d4975a9ce73c0ce5bc0ea2a
192Iridium77 <matt.martin0108@live.com>
2017-07-21 21:36:43 +0930

    Added Weather Feed

--- headlines.py
+++ headlines.py
@@ -3,8 +3,9 @@ from flask import Flask
 from flask import render_template
 from flask import request
 import json
-import urllib3
 import urllib
+import urllib.parse
+from urllib.request import urlopen
 
 app = Flask(__name__)
 
@@ -22,8 +23,24 @@ def get_news():
     else:
         publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
+    weather = get_weather("Adelaide,AU")
 
-    return render_template("home.html", articles=feed['entries'])
+    return render_template("home.html", articles=feed['entries'], weather=weather)
+
+
+def get_weather(query):
+    api_url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=83eeda99c5495de62d6086f706f7f1bf"
+    query = urllib.parse.quote(query)
+    url = api_url.format(query)
+    data = urlopen(url).read()
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get("weather"):
+        weather = {"description": parsed["weather"][0]["description"],
+                   "temperature":parsed["main"]["temp"],
+                   "city":parsed["name"]
+        }
+    return weather
 
 
 if __name__ == '__main__':
